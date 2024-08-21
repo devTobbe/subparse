7868dc31d04d030d271bfa9bce383b07a8aa651b
devTobbe <32337476+devTobbe@users.noreply.github.com>
2024-08-21 15:27:17 +0200

    feat: add parseSRT functionality

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,35 @@
-/** Function that parses .srt data after file is handled*/
-function parseSRT(data: string) {
-  const subs = data.split("\n\n");
-  return subs.map((subs) => {
-    const splits = subs.split("\n");
-    const line = splits[0];
-    const time = splits[1].split("-->");
-    const start = time[0];
-    const end = time[1];
-    const text = splits.slice(2).join();
+interface Parsed {
+  line: number;
+  start: string;
+  end: string;
+  text: string;
+}
 
-    return { line, start, end, text };
+/** Function that parses .srt data after file is handled
+ * Notes: Hopefully works now but the final returned value is an array of strings of JSON objects. The whole thing should be JSON
+ * */
+export function parseSRT(data: string): string {
+  /** Might be just \n*/
+  const subs = data.split("\n\n");
+  const mapped = subs.map((subs) => {
+    const splits = subs.split("\n");
+    const index = splits[0];
+    const time = splits[1].split("-->");
+    const initial = time[0].trim();
+    const final = time[1].trim();
+    const line = splits.slice(2).join(" ");
+
+    const parsed: Parsed = {
+      line: parseInt(index),
+      start: initial,
+      end: final,
+      text: line,
+    };
+
+    return parsed;
   });
+
+  return JSON.stringify(mapped);
 }
 
 /** Function that parses .ass data after file is handled*/
@@ -22,7 +41,3 @@ function parseSSA(data: string) { }
 /** */
 function handleFile(filePath: string) { }
 
-module.exports = {
-  parseSRT,
-  handleFile,
-};
