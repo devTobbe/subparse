0c3dc7df0de14acb02a5ecf1b4934deb77b7006d
devTobbe <32337476+devTobbe@users.noreply.github.com>
2024-09-03 15:50:39 +0200

    test: add more tests

--- tests/parseSRT.test.ts
+++ tests/parseSRT.test.ts
@@ -1,7 +1,47 @@
 // tests/index.test.ts
 import { parseSRT } from "../src/index";
 
+//Test a generic .srt test file
 test("parseSRT should correctly parse SRT data with multiple lines of text", () => {
+  const srtData = `1
+00:00:01,000 --> 00:00:04,000
+Hello, world!
+
+2
+00:00:05,000 --> 00:00:07,000
+Hello again!
+
+3
+00:00:08,000 --> 00:00:10,000
+Single line subtitle.`;
+
+  const expectedOutput = JSON.stringify([
+    {
+      line: 1,
+      start: "00:00:01,000",
+      end: "00:00:04,000",
+      text: "Hello, world!",
+    },
+    {
+      line: 2,
+      start: "00:00:05,000",
+      end: "00:00:07,000",
+      text: "Hello again!",
+    },
+    {
+      line: 3,
+      start: "00:00:08,000",
+      end: "00:00:10,000",
+      text: "Single line subtitle.",
+    },
+  ]);
+
+  expect(parseSRT(srtData)).toBe(expectedOutput);
+});
+
+
+//Test a .srt file with multiple lines per subtitle
+test("parseSRT should correctly parse subtitles with multiple lines", () => {
   const srtData = `1
 00:00:01,000 --> 00:00:04,000
 Hello, world!
@@ -39,3 +79,27 @@ Single line subtitle.`;
 
   expect(parseSRT(srtData)).toBe(expectedOutput);
 });
+
+//Test input with no content.
+test("parseSRT should return an empty array for empty input", () => {
+  const srtData = "";
+  const expectedOutput = JSON.stringify([]);
+  expect(parseSRT(srtData)).toBe(expectedOutput);
+});
+
+//Test single line subtitle file
+test("parseSRT should correctly parse single line subtitles", () => {
+  const srtData = `1
+00:00:01,000 --> 00:00:04,000
+Hello, world!`;
+  const expectedOutput = JSON.stringify([
+    {
+      line: 1,
+      start: "00:00:01,000",
+      end: "00:00:04,000",
+      text: "Hello, world!",
+    },
+  ]);
+  expect(parseSRT(srtData)).toBe(expectedOutput);
+});
+
